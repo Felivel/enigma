c3f4502b6c676fc6549f55c0bab213f384818729
Felivel Camilo <felivel.camilo@gmail.com>
2019-06-29 16:53:17 -0400

    Added instance id

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import hashlib
 import uuid
 
 app = Flask(__name__)
+instance_id = str(uuid.uuid4())
 
 @app.route('/')
 def index():
@@ -14,27 +15,27 @@ def user(name):
 
 @app.route('/square/<int:number>')
 def square(number):
-    response = jsonify({'result': number*number})
+    response = jsonify({'instance_id':instance_id, 'result': number*number})
     response.status_code = 200
     return response
 
 @app.route('/uuid')
 def generate_uuid():
     result = str(uuid.uuid4())
-    response = jsonify({'result': result})
+    response = jsonify({'instance_id':instance_id, 'result': result})
     response.status_code = 200
     return response
 
 @app.route('/sha1/<string:text>')
 def sha1(text):
     result = hashlib.sha1(text.encode())
-    response = jsonify({'result': result.hexdigest()})
+    response = jsonify({'instance_id':instance_id, 'result': result.hexdigest()})
     response.status_code = 200
     return response
 
 @app.route('/sha256/<string:text>')
 def sha256(text):
     result = hashlib.sha256(text.encode())
-    response = jsonify({'result': result.hexdigest()})
+    response = jsonify({'instance_id':instance_id, 'result': result.hexdigest()})
     response.status_code = 200
     return response
